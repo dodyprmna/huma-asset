d6179950d34f8096f959db070ef25c7f29919741
Dody <dodyprmna6@gmail.com>
2023-06-27 15:00:42 +0700

    model level dan model unit

---
+++ app/Models/Unit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Unit extends Model
+{
+    use SoftDeletes;
+    protected $table        = 'call_center';
+    protected $primarykey   ='id_call_center';
+
+}
